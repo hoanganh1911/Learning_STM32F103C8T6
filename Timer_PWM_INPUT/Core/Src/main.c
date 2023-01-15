151be4e0812f408b77da215b2de425900556769a
hoanganh1911 <hoanganh9637@gmail.com>
2023-01-15 19:14:21 +0700

    1Mhz do dc 101Hz

--- Timer_PWM_INPUT/Core/Src/main.c
+++ Timer_PWM_INPUT/Core/Src/main.c
@@ -128,7 +128,7 @@ int main(void)
   HAL_TIM_IC_Start(&htim2, TIM_CHANNEL_2);   // indirect channel
   HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
 //  __HAL_TIM_SET_COMPARE(&htim4,TIM_CHANNEL_1,50);
-  TIM4->CCR1 = 500;
+  TIM4->CCR1 = 5000;
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -278,7 +278,7 @@ static void MX_TIM4_Init(void)
   htim4.Instance = TIM4;
   htim4.Init.Prescaler = 71;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 999;
+  htim4.Init.Period = 9999;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
